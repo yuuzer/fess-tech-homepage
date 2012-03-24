ff3a14c0f7356c8c07340baa548e009013eb3e9f
yuuzer <csadam85@gmail.com>
2012-03-24 15:48:21 +0100

    UPD: gallery reconf + css

--- WebContent/js/jquery.galleriffic.conf.js
+++ WebContent/js/jquery.galleriffic.conf.js
@@ -2,7 +2,7 @@ jQuery(document).ready(
 		function($) {
 			// We only want these styles applied when javascript is enabled
 			$('div.navigation').css({
-				'width' : '300px',
+				'width' : '320px',
 				'float' : 'left'
 			});
 			$('div.content').css('display', 'block');
